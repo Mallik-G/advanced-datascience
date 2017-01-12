cab34aef692650d2b99ce9c9db05b351bba520e2
Algorithmica <info@algorithmica.co.in>
2017-01-12 08:09:08 -0600

    uploading tensorflow basic code and python functions

---
+++ 4.objective perceptron/classification-sigmoid-perceptron-v1.py
@@ -0,0 +1,52 @@
+# Import required packages
+from tensorflow.contrib import learn
+from tensorflow.contrib import layers
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import seaborn as sb
+
+# Set logging level to info to see detailed log output
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# Read the train data
+sample = pd.read_csv("train.csv")
+sample.shape
+sample.info()
+
+FEATURES = ['x1','x2']
+LABEL = ['label']
+
+sb.swarmplot(x='x1', y='x2', data=sample, hue='label', size=10)
+
+# Preprocess input data
+def normalize(x) :    
+    return ( (x - np.mean(x)) / (np.max(x) - np.min(x)) )
+    
+def input_function(dataset, train=False):
+    dataset.x1 = normalize(dataset.x1)
+    dataset.x2 = normalize(dataset.x2)
+    feature_cols = {k : tf.constant(dataset[k].values) 
+                        for k in FEATURES}
+    if train:
+        labels = tf.constant(dataset[LABEL].values)
+        return feature_cols, labels
+    return feature_cols
+    
+# Build the model with right feature tranformation
+feature_cols = [layers.real_valued_column(k) for k in FEATURES]
+
+classifier = learn.LinearClassifier(feature_columns=feature_cols,
+                                            n_classes=2,
+                                            model_dir="/tmp/model1")              
+classifier.fit(input_fn = lambda: input_function(sample,True), steps=1000)
+
+classifier.weights_
+classifier.bias_
+
+# Predict the outcome using model
+dict = {'x1':[10.4,21.5,10.5], 'x2':[22.1,26.1,2.7] }
+test = pd.DataFrame.from_dict(dict)
+
+predictions = classifier.predict(input_fn = lambda: input_function(test,False))
+predictions
